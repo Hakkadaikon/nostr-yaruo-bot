3733657b20ec6cef108a1f497736923417dc9382
hakkadaikon <hakkadaikon@yahoo.co.jp>
2023-06-28 07:29:14 +0900

    Organize code.

--- src/actions/news.js
+++ src/actions/news.js
@@ -2,20 +2,20 @@ const NewsAPI = require("newsapi");
 const config = require("../utils/config.js");
 
 const getGameNews = (callback) => {
-    // NewsAPI
-    const newsapi = new NewsAPI(config.BOT_NEWS_API_KEY);
+  // NewsAPI
+  const newsapi = new NewsAPI(config.BOT_NEWS_API_KEY);
 
-    // キーワード検索
-    newsapi.v2
-        .everything({
-            q: "ゲーム",
-        })
-        .then((response) => {
-            var news = response.articles;
-            callback(news);
-        });
+  // キーワード検索
+  newsapi.v2
+    .everything({
+      q: "ゲーム",
+    })
+    .then((response) => {
+      var news = response.articles;
+      callback(news);
+    });
 };
 
 module.exports = {
-    getGameNews,
+  getGameNews,
 };
